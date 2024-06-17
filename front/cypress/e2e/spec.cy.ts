304885dc908772aeb3f6d0f869249c7ea1d11171
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-06-17 14:25:21 +0200

    first

--- front/cypress/e2e/spec.cy.ts
+++ front/cypress/e2e/spec.cy.ts
@@ -1,15 +1,45 @@
-describe('Aperitif testing', () => {
+// describe('test1', () => {
+//   it('response 200', () => {
+//     cy.request({
+//       url: 'http://localhost:8080/api/est-ce-que-c-est-l-heure-de-l-apero',
+//       timeout: 300
+//     }).then((response) => {
+//
+//       expect(response.status).to.eq(200);
+//       expect(response.duration).to.be.lessThan(300);
+//       expect(response.body).to.have.length.of.at.least(5);
+//     });
+//   });
+// });
+//
+// describe('test 2',() => {
+//   it('Vérifier que le composant affiche ...', () => {
+//     cy.intercept({
+//       url: '**/api/est-ce-que-c-est-l-heure-de-l-apero'
+//     }).as('apero')
+//     cy.visit('http://localhost:4200')
+//     cy.get('#1').contains('...')
+//
+//     cy.wait('@apero').then((interception) => {
+//       // @ts-ignore
+//       expect(interception.response.body).to.not.be.null;
+//     })
+//
+//   })
+// })
 
-  it('/api/est-ce-que-c-est-l-heure-de-l-apero - works', () => {
-    // ...
-  })
+describe('test 3', () => {
+  it('Une erreur est affichée en cas de déconnexion du backend', () => {
+    cy.intercept(
+      '**/api/est-ce-que-c-est-l-heure-de-l-apero',
+      { statusCode: 500 }
+    ).as('getServerFailure')
+    cy.visit('http://localhost:4200')
+    cy.wait('@getServerFailure')
 
-  it('Checks that request response is displayed within a h1 title', () => {
-    // ...
-  })
+    cy.get('h2').first().contains('Service indisponible :/')
 
-  it('Backend Errors are managed (service unavailable)', () => {
-    // ...
-  })
+  });
+
 
 })
--- front/cypress/support/e2e.ts
+++ front/cypress/support/e2e.ts
@@ -1,4 +1,4 @@
-// ***********************************************************q
+// ***********************************************************
 // This example support/e2e.ts is processed and
 // loaded automatically before your test files.
 //
